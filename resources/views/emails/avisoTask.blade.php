9c2b5b3915858b43124a9317c3c587229ae65c1c
angeldavidbravo <angeldavidve@gmail.com>
2023-04-23 10:28:49 -0300

    pagina presentacion sin editar

--- resources/views/emails/avisoTask.blade.php
+++ resources/views/emails/avisoTask.blade.php
@@ -1,27 +1,20 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>avisoMail</title>
-
+<!doctype html>
+<html>
+  <head>
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+  </head>
+  <body style="font-family: sans-serif;">
+    <div style="display: block; margin: auto; max-width: 600px;" class="main">
+      <h1 style="font-size: 18px; font-weight: bold; margin-top: 20px">Congrats for sending test email with Mailtrap!</h1>
+      <p>Inspect it using the tabs you see above and learn how this email can be improved.</p>
+      <img alt="Inspect with Tabs" src="https://assets-examples.mailtrap.io/integration-examples/welcome.png" style="width: 100%;">
+      <p>Now send your email using our fake SMTP server and integration of your choice!</p>
+      <p>Good luck! Hope it works.</p>
+    </div>
+    <!-- Example of invalid for email html/css, will be detected by Mailtrap: -->
     <style>
-        body{
-            background-position: center
-
-        }
+      .main { background-color: white; }
+      a:hover { border-left-width: 1em; min-height: 2em; }
     </style>
-
-    
-</head>
-<body>
-    <h1>correo de recordatorio</h1>
-    <p>este es un correo con el fin de recordarle que tiene una tarea en curso que culminara pronto</p>
-    
-    <p><strong>Tarea:<strong></p>
-    <p><strong>Descripcion:<strong></p>
-    <p><strong>Fecha de culminacion:<strong></p>
-    
-</body>
+  </body>
 </html>
